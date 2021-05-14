d362b37139a9ddfdb8be6da2747b541a9ea6fa88
Raul <raulrufino0@gmail.com>
2021-05-14 14:15:38 -0300

    feat: Adicione estrutura inicial das tarefas

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,8 +1,37 @@
 from celery import Celery
+from pandas import read_csv
+
 
 app = Celery('tasks.tasks')
 
+
 @app.task
-def add(x, y):
-    return x + y
+def save_dataset(df):
+    pass
+
+
+@app.task
+def save_csv(csv_path, **kargs):
+    df = read_csv(csv_path, **kargs)
+    save_dataset(df)
+
+
+@app.task
+def save_excel(excel_path, **kargs):
+    df = read_csv(excel_path, **kargs)
+    save_dataset(df)
+
+
+
+def select_parser(file_path):
+    pass
+
+
+@app.task
+def read_files(data_path):
+    files_path = []
+    for file_path in files_path:
+        parser, kargs = select_parser(file_path)
+        parser.delay(file_path, **kargs)
+
 
