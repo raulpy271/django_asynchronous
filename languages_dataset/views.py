314d91baf15ef72607439a498d8a366bd9cf6aff
Raul <raulrufino0@gmail.com>
2021-05-14 22:30:53 -0300

    feat: Adicione no db em todo request
    Esta feature eh somente para testar a sincronia do db nos dois containers

--- languages_dataset/views.py
+++ languages_dataset/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_list_or_404
 from .models import Language
+from .tasks.tasks import save_example
 
 
 def index(request):
+    save_example()
     languages = get_list_or_404(Language)
     context = {'languages': languages}
     return render(request, 'index.html', context)
