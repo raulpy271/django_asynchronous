7e02764d6f7b01bf3866c28ec752c1ef4fbd9cc0
Raul <raulrufino0@gmail.com>
2021-05-17 17:28:13 -0300

    feat: Adicione rota para servir todas as linguagens em json

--- languages_dataset/urls.py
+++ languages_dataset/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('data', views.data, name='data'),
 ]
 
--- languages_dataset/views.py
+++ languages_dataset/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Language
+from .serializers import LanguageSerializer
 from .tasks import read_files
 
 
@@ -19,5 +23,13 @@ def index(request):
         'languages': languages, 
         'render_table': render_table}
     return render(request, 'index.html', context)
+
+
+@api_view(['GET'])
+def data(request):
+    if request.method == 'GET':
+        languages = Language.objects.all()
+        languages_data = LanguageSerializer(languages, many=True).data
+        return Response(languages_data)
 
 
