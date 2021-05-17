282315fb1a9030896768566a228348fd38a49dc4
Raul <raulrufino0@gmail.com>
2021-05-17 18:56:27 -0300

    feat: Adicione condicional para processar os dados apenas uma vez

--- languages_dataset/views.py
+++ languages_dataset/views.py
@@ -7,13 +7,20 @@ from .serializers import LanguageSerializer
 from .tasks import read_files
 
 
+was_rendering = False
+
+
 def index(request):
     return render(request, 'index.html', {})
 
 
 @api_view(['GET'])
 def data(request):
+    global was_rendering
     if request.method == 'GET':
+        if not was_rendering:
+            read_files.delay()
+            was_rendering = True
         languages = Language.objects.all()
         languages_data = LanguageSerializer(languages, many=True).data
         return Response(languages_data)
