7f286143b95a9f387d3bb92910cd78d2ffd4e60c
Raul <raulrufino0@gmail.com>
2021-05-22 16:00:41 -0300

    feat: Adicione constante com nomes das colunas necessarias

--- languages_dataset/models.py
+++ languages_dataset/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
+columns = ('name', 'year', 'paradigm', 'site')
+
+
 class Language(models.Model):
     name = models.CharField(max_length=25)
     year = models.PositiveSmallIntegerField()
